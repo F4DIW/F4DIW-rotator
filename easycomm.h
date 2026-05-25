4771f39e5caa88df668401ee40f25749fa6c7e49
F4DIW <f4diwqsl@gmail.com>
2026-05-25 09:37:40 +0200

    Fix Look4Sat BT parsing - split on P delimiter, full tracking working

--- easycomm.h
+++ easycomm.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include "BluetoothSerial.h"
+#include "esp_bt_device.h"
 #include "globals.h"
 
 #define BUFFER_SIZE 256
@@ -17,11 +18,36 @@ public:
     void easycomm_init() {
         Serial.begin(9600);
         delay(500);
+
+        SerialBT.register_callback([](esp_spp_cb_event_t event, esp_spp_cb_param_t* param) {
+            if (event == ESP_SPP_SRV_OPEN_EVT) Serial.println("[BT] Connected");
+            if (event == ESP_SPP_CLOSE_EVT)    Serial.println("[BT] Disconnected");
+        });
+
         SerialBT.begin(BT_DEVICE_NAME);
+
+        const uint8_t* mac = esp_bt_dev_get_address();
+        char macStr[18];
+        snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
         Serial.println("[INFO] EasyComm ready - USB + BT");
+        Serial.print("[INFO] BT Name: ");
+        Serial.println(BT_DEVICE_NAME);
+        Serial.print("[INFO] BT MAC:  ");
+        Serial.println(macStr);
     }
 
     void easycomm_proc() {
+        // Envoyer position toutes les secondes vers BT
+        static uint32_t last_report = 0;
+        if (millis() - last_report > 1000) {
+            String pos = "AZ" + String(control_az.setpoint, 1) +
+                         " EL" + String(control_el.setpoint, 1) + "\n";
+            SerialBT.print(pos);
+            Serial.print("[TX] " + pos);
+            last_report = millis();
+        }
+
         if (SerialBT.available()) {
             _active_source = SOURCE_BT;
             _read_and_process(SerialBT);
@@ -41,11 +67,19 @@ private:
         while (src.available()) {
             char c = (char)src.read();
             if (c == '\n' || c == '\r') {
+                // Fin de trame normale
                 if (_buf_cnt > 0) {
                     _buffer[_buf_cnt] = '\0';
                     _process_cmd(_buffer);
                     _buf_cnt = 0;
                 }
+            } else if (c == 'P' && _buf_cnt > 0) {
+                // Look4Sat envoie sans \n entre les trames
+                // On detecte le debut d un nouveau paquet P
+                _buffer[_buf_cnt] = '\0';
+                _process_cmd(_buffer);
+                _buf_cnt = 0;
+                _buffer[_buf_cnt++] = c;
             } else {
                 if (_buf_cnt < BUFFER_SIZE - 1)
                     _buffer[_buf_cnt++] = c;
@@ -62,10 +96,26 @@ private:
         String str = String(cmd);
         str.trim();
 
+        // Look4Sat format: "P az el"
+        if (str.startsWith("P ")) {
+            int space1 = str.indexOf(' ');
+            int space2 = str.indexOf(' ', space1 + 1);
+            if (space1 >= 0 && space2 >= 0) {
+                control_az.setpoint = constrain(
+                    str.substring(space1 + 1, space2).toFloat(),
+                    MIN_M1_ANGLE, MAX_M1_ANGLE);
+                control_el.setpoint = constrain(
+                    str.substring(space2 + 1).toFloat(),
+                    MIN_M2_ANGLE, MAX_M2_ANGLE);
+                Serial.println("[L4S] AZ=" + String(control_az.setpoint, 1) +
+                               " EL=" + String(control_el.setpoint, 1));
+            }
+            return;
+        }
+
         // EasyComm I : "AZxxx.x ELxxx.x" sur une seule ligne
         int az_idx = str.indexOf("AZ");
         int el_idx = str.indexOf("EL");
-
         if (az_idx >= 0 && el_idx >= 0) {
             control_az.setpoint = constrain(
                 str.substring(az_idx + 2, el_idx).toFloat(),
@@ -87,6 +137,9 @@ private:
                 control_el.setpoint = constrain(str.substring(2).toFloat(), MIN_M2_ANGLE, MAX_M2_ANGLE);
             else
                 _send("EL" + String(control_el.input, 1) + "\n");
+        } else if (str.startsWith("AZ EL")) {
+            _send("AZ" + String(control_az.input, 1) +
+                  " EL" + String(control_el.input, 1) + "\n");
         } else if (str.startsWith("SA")) {
             control_az.setpoint = control_az.input;
         } else if (str.startsWith("SE")) {
@@ -98,7 +151,8 @@ private:
         } else if (str.startsWith("VE")) {
             _send("VE001\n");
         } else if (str.startsWith("IP")) {
-            _send("AZ" + String(control_az.input, 1) + " EL" + String(control_el.input, 1) + "\n");
+            _send("AZ" + String(control_az.input, 1) +
+                  " EL" + String(control_el.input, 1) + "\n");
         } else if (str.startsWith("RB")) {
             delay(100);
             ESP.restart();
